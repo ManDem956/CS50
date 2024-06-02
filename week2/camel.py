9328ebf1390896464d6e9d4a1eb4353861812591
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 00:58:06 +0000

    Sun, Jun 2, 2024, 3:58 AM +03:00

---
+++ week2/camel.py
@@ -0,0 +1,10 @@
+from typing import NoReturn
+from string import ascii_upp    
+
+def main() -> NoReturn:
+    user_input: str = input("Please provide a camelCase variable: ").strip().lower()
+
+
+
+if __name__ == "__main__":
+    main()
