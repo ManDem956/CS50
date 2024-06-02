2087415dc9b80ca0e582b6ae93ffa51f4b77a39b
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 14:35:55 +0000

    Sun, Jun 2, 2024, 5:35 PM +03:00

--- week4/game/game.py
+++ week4/game/game.py
@@ -5,14 +5,15 @@ CONST_USER_PROMT = "Name"
 
 
 def get_user_input(message: str, sep: str = ": ") -> str:
-    res: str = input(f"{message}{sep}").strip()
+    return input(f"{message}{sep}").strip()
 
-def validate_input_int(value: int) -> bool
-    while True:
+def validate_input_int(value: str) -> bool:
+    return value.isdigit()
 
 
 def main() -> NoReturn:
-    user_input = get_user_input(CONST_USER_PROMT)
+    while validate_input_int(user_input := get_user_input(CONST_USER_PROMT)):
+        pass
 
     print(f"{user_input}")
 
