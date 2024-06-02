4987e0e7e3b04aff25e47d290bb736303329c895
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 02:42:56 +0000

    Sun, Jun 2, 2024, 5:42 AM +03:00

---
+++ week2/nutrition/nutrition.py
@@ -0,0 +1,39 @@
+from typing import NoReturn
+
+CONST_USER_PROMT = "Item"
+CONST_FRUIT_MAPPING = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew Melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet Cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
+}
+
+
+def get_user_input(str) -> str:
+    res: str = input(f"{str}: ").strip()
+    return res
+
+
+def main() -> NoReturn:
+    input = get_user_input(CONST_USER_PROMT)
+    print(f"Calories: {CONST_FRUIT_MAPPING[input]}")
+
+
+if __name__ == "__main__":
+    main()
