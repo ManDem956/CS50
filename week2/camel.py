c58d6fead4fd0d4d5b0a4fcf779d18af0f90b108
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 01:03:53 +0000

    Sun, Jun 2, 2024, 4:03 AM +03:00

--- week2/camel.py
+++ week2/camel.py
@@ -1,8 +1,13 @@
 from typing import NoReturn
-from string import ascii_upp    
+from string import ascii_uppercase as caps
 
 def main() -> NoReturn:
-    user_input: str = input("Please provide a camelCase variable: ").strip().lower()
+    user_input: str = input("Please provide a camelCase variable: ").strip()
+    indices = []
+    for idx, character in enumerate(user_input):
+        if character in caps:
+            indices.append(idx)
+    print(f"{indices=}")
 
 
 
