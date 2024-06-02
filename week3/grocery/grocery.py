63e69ca405c1aaa302341c8ae404e4f5aca7a231
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 11:46:23 +0000

    Sun, Jun 2, 2024, 2:46 PM +03:00

--- week3/grocery/grocery.py
+++ week3/grocery/grocery.py
@@ -4,8 +4,9 @@ from collections import defaultdict
 CONST_USER_PROMT = ""
 
 
-def get_user_input(str) -> str:
-    res: str = input().strip()
+
+def get_user_input(message:str, sep:str=": ") -> str:
+    res: str = input(f"{message}{sep}").strip()
     return res
 
 
@@ -13,7 +14,7 @@ def main() -> NoReturn:
     groceries = defaultdict(int)
     while True:
         try:
-            input = get_user_input(CONST_USER_PROMT).upper()
+            input = get_user_input(CONST_USER_PROMT, "").upper()
             groceries[input] += 1
         except EOFError:
             print()
