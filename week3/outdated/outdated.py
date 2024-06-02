6ead154431ce75e269fdb8c40f0c65b152286736
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 12:03:22 +0000

    Sun, Jun 2, 2024, 3:03 PM +03:00

--- week3/outdated/outdated.py
+++ week3/outdated/outdated.py
@@ -9,15 +9,15 @@ def get_user_input(message:str, sep:str=": ") -> str:
     res: str = input(f"{message}{sep}").strip()
     return res
 
-def get_us_date_numerical(value:str) -> tuple[int, int, int]:
-    month, day, tear = (int(element) for element in value.split("/"))
+def get_us_date_numerical(value:str) -> tuple[str, str, str]:
+    month, day, year = (int(element) for element in value.split("/"))
 
     if day >31:
         raise ValueError("Day can not be larger han 31")
 
-    return month, day, tear
+    return f"{month:>02}", f"{day:>02}"
 
-def get_iso_date(year:int, month:int, day:int) -> str:
+def get_iso_date(year:str, month:str, day:str) -> str:
     return "-".join((year, month, day))
 
 
