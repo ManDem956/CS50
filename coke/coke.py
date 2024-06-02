86eafebc4e9d5261a5eac7dfc3b91747cb2953c3
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 01:21:58 +0000

    Sun, Jun 2, 2024, 4:21 AM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,18 +4,20 @@ CONST_START_AMOUNT_DUE = 50
 CONST_ACCEPTED_COINS = [25, 10, 5]
 CONST_USER_PROMT = "Please provide a camelCase variable"
 
+
 def get_user_input(str) -> int:
     res: int = int(input(f"{str}: "))
     if res not in CONST_ACCEPTED_COINS:
-        raise ValueError("Accepted coins are: {CONST_ACCEPTED_COINS}")
+        return 0
     return res
 
 
 def main() -> NoReturn:
     amount_due = CONST_START_AMOUNT_DUE
-    while amount_due >0:
+    while amount_due > 0:
+        print(f"Amount due: {amount_due}")
         amount_due -= get_user_input(CONST_USER_PROMT)
-    
+    print(f"Change Owed : {abs(amount_due)}")
 
 
 if __name__ == "__main__":
