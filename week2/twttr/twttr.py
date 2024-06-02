432090d16b56f7941c3792a69e278fb224be49e8
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 01:35:49 +0000

    Sun, Jun 2, 2024, 4:35 AM +03:00

---
+++ week2/twttr/twttr.py
@@ -0,0 +1,23 @@
+from typing import NoReturn
+
+CONST_VOWELS = "AEIOU"
+CONST_USER_PROMT = "Insert coin"
+
+
+def get_user_input(str) -> str:
+    res: str = input(f"{str}: ").strip()
+    return res
+
+
+def main() -> NoReturn:
+    input = get_user_input("Input")
+    result = ""
+    for character in input:
+        if character.upper() not in CONST_VOWELS:
+            result += character
+
+    print(f"Output: {result}")
+
+
+if __name__ == "__main__":
+    main()
