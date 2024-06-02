67b6ff800144b1ecc2c1838cff602a1a869d5c81
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 12:00:19 +0000

    Sun, Jun 2, 2024, 3:00 PM +03:00

--- week3/outdated/outdated.py
+++ week3/outdated/outdated.py
@@ -9,20 +9,21 @@ def get_user_input(message:str, sep:str=": ") -> str:
     res: str = input(f"{message}{sep}").strip()
     return res
 
-def get_us_dage_numerical(value:str) -> tuple[int, int, int]:
-    month, day, tear = value.split("/")
+def get_us_date_numerical(value:str) -> tuple[int, int, int]:
+    month, day, tear = (int(element) for element in value.split("/"))
 
     if day >31:
         raise ValueError("Day can not be larger han 31")
 
     return month, day, tear
 
+def get_iso_date(year:int, month:int, day:int) -> str:
+    return "-".join((year, month, day))
 
 
 def main() -> NoReturn:
-    groceries = defaultdict(int)
     input = get_user_input(CONST_USER_PROMT)
-
+    print(get_iso_date(*get_us_date_numerical(input)))
 
 if __name__ == "__main__":
     main()
