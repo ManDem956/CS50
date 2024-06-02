eff646dbd541b3385d7aa81c687b5c4c48f32638
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 09:40:58 +0000

    Sun, Jun 2, 2024, 12:40 PM +03:00

--- week3/fuel/fuel.py
+++ week3/fuel/fuel.py
@@ -2,15 +2,31 @@ from typing import NoReturn
 
 CONST_USER_PROMT = "Fraction"
 
+
 def get_user_input(str) -> str:
     res: str = input(f"{str}: ").strip()
     return res
 
 
+def convert_to_int(value: str) -> int:
+    return int(value)
+
+
+def parse_fraction(input: str) -> tuple[int, int]:
+    values = input.split('/')
+    if len(values) != 2:
+        raise ValueError(
+            f"A fraction rewuires divident and divisor. Expected 2 value, got {len(values)}")
+
+    return int(values[0]), int(values[1])
+
+
 def main() -> NoReturn:
     input = get_user_input(CONST_USER_PROMT).lower()
-    if input in CONST_FRUIT_MAPPING:
-        print(f"Calories: {CONST_FRUIT_MAPPING.get(input.lower(),"")}")
+
+    divident, divisor = parse_fraction(input)
+
+    print((divident / divisor) * 100)
 
 
 if __name__ == "__main__":
