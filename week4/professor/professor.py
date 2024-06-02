6f467b7d2b987b0a75a0c2e6178efb9f4e767bc0
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 18:29:27 +0000

    Sun, Jun 2, 2024, 9:29 PM +03:00

--- week4/professor/professor.py
+++ week4/professor/professor.py
@@ -12,7 +12,7 @@ def get_user_input(message: str, sep: str = ": ") -> str:
     return input(f"{message}{sep}").strip()
 
 
-def get_level(message: str) -> int:
+def get_level(message: str = CONST_USER_PROMT_LEVEL) -> int:
     result = 0
     while not result in range(1,4):
         try:
@@ -23,7 +23,10 @@ def get_level(message: str) -> int:
     return result
 
 
-
+def generate_integer(level: int) -> int:
+    if level not in range(1,4):
+        raise ValueError("Level must in range [1..3] inclusive")
+    
 
 
 def main() -> NoReturn:
