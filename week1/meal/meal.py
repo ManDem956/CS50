261980f31c48f06dc849fa53a20084de3f75ff72
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 00:45:44 +0000

    Sun, Jun 2, 2024, 3:45 AM +03:00

--- week1/meal/meal.py
+++ week1/meal/meal.py
@@ -1,26 +1,24 @@
 from typing import NoReturn
 
-CONST_TIME_RANGES = {(7.0, (8.0*60+1)/60): "breakfast"
-                     (12,0, (13*60+1)/60): "lunch"
-                     (18.0, (18*60+1)/60): "dinner"}
-
 
 def convert(time: str) -> float:
     hours, minutes = time.split(":")
     return (float(hours) * 60 + float(minutes)) / 60
 
 
-def main() -> NoReturn
+def main() -> NoReturn:
     user_input: str = input("What time is it?").strip().lower()
     result = ""
     match convert(user_input):
-        case num if 7.0 < num <=8.0:
+        case num if 7.0 < num <= 8.0:
             result = "breakfast"
-        case num if 12.0 < num <=13.0:
+        case num if 12.0 < num <= 13.0:
             result = "lunck"
-        case num if 18.0 < num <=19.0:
+        case num if 18.0 < num <= 19.0:
             result = "dinner"
-    print(CON)
+
+    if result:
+        print(f"{result} time")
 
 
 if __name__ == "__main__":
