97abedd9b732973a6abe50b7dfcb05f09f01004b
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 11:03:05 +0000

    Sun, Jun 2, 2024, 2:03 PM +03:00

---
+++ week3/taqueria/taqueria.py
@@ -0,0 +1,35 @@
+from typing import NoReturn
+
+CONST_USER_PROMT = "Item"
+CONST_MENU = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def get_user_input(str) -> str:
+    res: str = input(f"{str}: ").strip()
+    return res
+
+
+def main() -> NoReturn:
+    total = 0
+    while True:
+        try:
+            input = get_user_input(CONST_USER_PROMT)
+            total += CONST_MENU.get(input.title())
+        except EOFError:
+            break
+
+    print(f"{Total:.2f}")
+
+
+
+if __name__ == "__main__":
+    main()
