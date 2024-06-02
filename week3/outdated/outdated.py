9ce969f2619a476f6349acb27ab4678eb820c848
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-02 12:05:01 +0000

    Sun, Jun 2, 2024, 3:05 PM +03:00

--- week3/outdated/outdated.py
+++ week3/outdated/outdated.py
@@ -4,26 +4,28 @@ from collections import defaultdict
 CONST_USER_PROMT = "Date"
 
 
-
-def get_user_input(message:str, sep:str=": ") -> str:
+def get_user_input(message: str, sep: str = ": ") -> str:
     res: str = input(f"{message}{sep}").strip()
     return res
 
-def get_us_date_numerical(value:str) -> tuple[str, str, str]:
+
+def get_us_date_numerical(value: str) -> tuple[str, str, str]:
     month, day, year = (int(element) for element in value.split("/"))
 
-    if day >31:
+    if day > 31:
         raise ValueError("Day can not be larger han 31")
 
     return f"{year:>04}", f"{month:>02}", f"{day:>02}"
 
-def get_iso_date(year:str, month:str, day:str) -> str:
+
+def get_iso_date(year: str, month: str, day: str) -> str:
     return "-".join((year, month, day))
 
 
 def main() -> NoReturn:
     input = get_user_input(CONST_USER_PROMT)
     print(get_iso_date(*get_us_date_numerical(input)))
+
 
 if __name__ == "__main__":
     main()
