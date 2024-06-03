276ef6a851602fc2c97f43f0e30b10d18f020093
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 14:36:30 +0000

    Mon, Jun 3, 2024, 5:36 PM +03:00

---
+++ week5/test_twttr/test_twttr.py
@@ -0,0 +1,12 @@
+from typing import NoReturn
+
+
+CONST_BITCOIN_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+
+def main() -> NoReturn:
+    pass
+
+
+if __name__ == "__main__":
+    main()
