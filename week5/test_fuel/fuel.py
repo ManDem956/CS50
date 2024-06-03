2561c486227d62d2cc6e6bb10c8e3e0e6c788e6c
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 19:33:44 +0000

    Mon, Jun 3, 2024, 10:33 PM +03:00

--- week5/test_fuel/fuel.py
+++ week5/test_fuel/fuel.py
@@ -8,7 +8,7 @@ def get_user_input(str) -> str:
     return res
 
 
-def convert(input: str) -> tuple[int, int]:
+def convert(input: str) -> int:
     # Will raise ValueError if int() conversion fails
     # Will raise ValueError if too many values to unpack
     divident, divisor = (int(value) for value in input.split('/'))
@@ -17,28 +17,27 @@ def convert(input: str) -> tuple[int, int]:
         raise ValueError(
             f"Divident can not be larger than divisor.")
 
-    return round(divident / divisor, 2)
+    return round((divident / divisor) * 100)
 
 
+def gauge(percentage: int) -> str:
+    if percentage in range(1, 99):
+        return f"{percentage}%"
+    elif percentage <= 1:
+        return "E"
+    else:
+        return "F"
 
 
 def main() -> NoReturn:
-    result = None
-    while result is None:
-        input = get_user_input(CONST_USER_PROMT).lower()
+    while True:
         try:
-            result = convert(input)
+            result = convert(get_user_input(CONST_USER_PROMT).lower())
         except (ValueError, ZeroDivisionError) as e:
             continue
-
-    message: str = ""
-    if 0.01 < result < 0.99:
-        message = f"{result:.0%}"
-    elif result <= 0.01:
-        message = "E"
-    else:
-        message = "F"
-    print(message)
+        else:
+            print(gauge(result))
+            break
 
 
 if __name__ == "__main__":
