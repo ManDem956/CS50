08828e00607272116f5f92863f15309b8a97bf84
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 00:49:27 +0000

    Mon, Jun 3, 2024, 3:49 AM +03:00

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -2,35 +2,18 @@ from typing import NoReturn
 import argparse
 import requests
 
-CONST_USER_PROMT_LEVEL = "Level"
 CONST_BITCOIN_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 
-def get_user_input(message: str, sep: str = ": ") -> str:
-    return input(f"{message}{sep}").strip()
-
-
-def get_level(message: str = CONST_USER_PROMT_LEVEL) -> int:
-    result = 0
-    while not result in range(1, 4):
-        try:
-            result = int(get_user_input(message))
-        except ValueError as e:
-            continue
-
-    return result
-
-
-def main():
+def main() -> NoReturn:
     parser = argparse.ArgumentParser(
-    prog="Bitcoin", description="Calculate the cost for users bitcoin amount")
+        prog="Bitcoin", description="Calculate the cost for user's bitcoin amount")
     parser.add_argument("amount", type=float)
     try:
         args = parser.parse_args()
     except SystemExit:
         sys.exit(1)
 
-        
     try:
         res = requests.get(CONST_BITCOIN_ENDPOINT)
         res.raise_for_status()
@@ -44,5 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
