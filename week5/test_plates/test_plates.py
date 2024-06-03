ffbcd953e4d8f7d21ce95ba6ac96a268c75cbd99
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 19:21:51 +0000

    Mon, Jun 3, 2024, 10:21 PM +03:00

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -3,9 +3,14 @@ from plates import is_valid
 
 
 @pytest.mark.parametrize("input,expected",
-                         [("CS50", 0),
-                          ("Hello, Newman", 0),
-                          ("How you doing?", 20),
-                          ("What's happening?", 100)])
-def test_value(input, expected):
-    assert value(input) == expected
+                         [("CS50", True),
+                          ("CS05", False),
+                          ("ECTO88", True),
+                          ("50", False),
+                          ("CS50P2", False),
+                          ("PI3.14", False),
+                          ("H", False),
+                          ("OUTATIME", False),
+                          ("NRVOUS", True)])
+def test_is_valid(input, expected):
+    assert is_valid(input) == expected
