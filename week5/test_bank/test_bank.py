918c7018a561dec57849c9e01895a083181115d0
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 19:12:00 +0000

    Mon, Jun 3, 2024, 10:12 PM +03:00

--- week5/test_bank/test_bank.py
+++ week5/test_bank/test_bank.py
@@ -4,9 +4,8 @@ from bank import value
 
 @pytest.mark.parametrize("input,expected",
                          [("Hello", 0),
-                          ("Hello, Newman", "Wht's yr nm?"),
-                          ("CS50", "CS50"),
-                          ("I am Groot", " m Grt"),
-                          ("PYTHON", "PYTHN")])
+                          ("Hello, Newman", 0),
+                          ("How you doing?", 20),
+                          ("What's happening?", 100)])
 def test_value(input, expected):
     assert value(input) == expected
