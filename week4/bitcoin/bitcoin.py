906bd61202bb5368a6e66838241497330685ef96
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 00:35:50 +0000

    Mon, Jun 3, 2024, 3:35 AM +03:00

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -30,7 +30,7 @@ def main(amount: float):
     except requests.RequestException as e:
         print("An HTTP error occured: {e}")
     else:
-        print(float(json["bpi"]["USD"]["rate"]) * amount)
+        print(float(json["bpi"]["USD"]["rate_float"]) * amount)
 
 
 if __name__ == "__main__":
