0513cf1919a9a62f5b66bd4237df70eea2d5618d
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 19:42:07 +0000

    Mon, Jun 3, 2024, 10:42 PM +03:00

--- week5/test_fuel/test_fuel.py
+++ week5/test_fuel/test_fuel.py
@@ -17,11 +17,11 @@ def test_convert(input, expected):
 @pytest.mark.parametrize("input,expected",
                          [(75, "75%"),
                           (33, "33%"),
-                          (67,"67"),
-                          ("0/100", 0),
-                          ("1/100", 1),
-                          ("100/100", 100),
-                          ("99/100", 99)
+                          (67,"67%"),
+                          (0, "E"),
+                          (1, "E"),
+                          (100, "F"),
+                          (99, "F")
                           ])
 def test_gauge(input, expected):
     assert gauge(input) == expected
