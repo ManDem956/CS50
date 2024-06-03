a89eac966f61b312fa99b02a5fe124e265c90066
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-03 19:25:16 +0000

    Mon, Jun 3, 2024, 10:25 PM +03:00

---
+++ week5/test_fuel/fuel.py
@@ -0,0 +1,44 @@
+from typing import NoReturn
+
+CONST_USER_PROMT = "Fraction"
+
+
+def get_user_input(str) -> str:
+    res: str = input(f"{str}: ").strip()
+    return res
+
+
+def parse_fraction(input: str) -> tuple[int, int]:
+    # Will raise ValueError if int() conversion fails
+    # Will raise ValueError if too many values to unpack
+    divident, divisor = (int(value) for value in input.split('/'))
+
+    if divident > divisor:
+        raise ValueError(
+            f"Divident can not be larger than divisor.")
+
+    return divident, divisor
+
+
+def main() -> NoReturn:
+    result = None
+    while result is None:
+        input = get_user_input(CONST_USER_PROMT).lower()
+        try:
+            divident, divisor = parse_fraction(input)
+            result = (divident / divisor)
+        except (ValueError, ZeroDivisionError) as e:
+            continue
+
+    message: str = ""
+    if 0.01 < result < 0.99:
+        message = f"{result:.0%}"
+    elif result <= 0.01:
+        message = "E"
+    else:
+        message = "F"
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
