1c23875c8007c71b91da20913efc735efba081c6
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-05 04:42:39 +0300

    week 7 - regexp - numb3rs

---
+++ week7/numb3rs/numb3rs.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+REGEXP = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+
+# REGEXP = r"\b(?:(?:25[0-5]?))\b"
+
+
+def validate(ip):
+    return re.fullmatch(REGEXP, ip) is not None
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ week7/numb3rs/test_numb3rs.py
@@ -0,0 +1,27 @@
+import pytest
+from numb3rs import validate
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("255.255.255.255", True),
+        ("0.0.0.0", True),
+        ("127.0.0.1", True),
+        ("255.255.255.255.", False),
+        (".255.255.255.255", False),
+        ("255.255.255.255.10", False),
+        ("255.256.257.258", False),
+        ("1.2.3.1000", False),
+        ("1.2.1000.1", False),
+        ("1.1000,200,173", False),
+        ("1000.2.3.1", False),
+        ("cat", False),
+        ("256", False),
+        ("255", False),
+        ("255.", False),
+        ("255.", False),
+    ],
+)
+def test_validate(input, expected):
+    assert validate(input) == expected
