b94a2f432fb9713a3858f4eccceefd0e55d56028
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-05 15:07:23 +0300

    week 7 - working

---
+++ week7/working/test_working.py
@@ -0,0 +1,21 @@
+import pytest
+from working import convert
+from contextlib import nullcontext as does_not_raise
+
+
+@pytest.mark.parametrize(
+    "input, expected, exception",
+    [
+        ("9:00 AM to 5:00 PM", "09:00 to 17:00", does_not_raise()),
+        ("9 AM to 5 PM", "09:00 to 17:00", does_not_raise()),
+        ("10 PM to 8 AM", "22:00 to 08:00", does_not_raise()),
+        ("10:00 PM to 8:00 AM", "22:00 to 08:00", does_not_raise()),
+        ("9:60 AM to 5:60 PM", None, pytest.raises(ValueError)),
+        ("9 AM - 5 PM", None, pytest.raises(ValueError)),
+        ("09:00 AM - 17:00 PM", None, pytest.raises(ValueError)),
+        ("cat", None, pytest.raises(ValueError)),
+    ],
+)
+def test_convert(input, expected, exception):
+    with exception:
+        assert convert(input) == expected
---
+++ week7/working/working.py
@@ -0,0 +1,41 @@
+import re
+
+
+REGEX_TIME_12 = r"(\b(?:1[0-2]|0?[1-9])(?::(?:[0-5][0-9])?)?\s(?:AM|PM)\b)(\sto\s)(\b(?:1[0-2]|0?[1-9])(?::(?:[0-5][0-9])?)?\s(?:AM|PM)\b)"
+
+
+def convert_12_to_24(value: str) -> str:
+    time, noon = value.split()
+    hours, _, minutes = time.partition(":")
+
+    if not minutes:
+        minutes = "00"
+
+    if noon == "PM":
+        hours = str(int(hours) + 12) if int(hours) < 12 else hours
+    else:
+        hours = str(int(hours) % 12)
+
+    return f"{hours:>02}:{minutes}"
+
+
+def do_replace(match: re.Match) -> str:
+    return f"{convert_12_to_24(match.group(1))}{match.group(2)}{convert_12_to_24(match.group(3))}"
+
+
+def convert(time_delta: str) -> str:
+    pattern = re.compile(REGEX_TIME_12)
+    res = re.sub(pattern, do_replace, time_delta)
+
+    if res == time_delta:
+        raise ValueError(f"Invalid time delta: {time_delta}")
+
+    return res
+
+
+def main():
+    print(convert(input("Time delta: ")))
+
+
+if __name__ == "__main__":
+    main()
