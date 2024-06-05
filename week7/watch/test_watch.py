dac2af36af6c1c04904d83d2a77e6c17ecbe3275
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-05 12:42:21 +0300

    week 7 - watch

---
+++ week7/watch/test_watch.py
@@ -0,0 +1,39 @@
+import pytest
+from watch import parse
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (
+            '<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>',
+            "https://youtu.be/xvFZjo5PgG0",
+        ),
+        (
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            "https://youtu.be/xvFZjo5PgG0",
+        ),
+        (
+            '<iframe src="https://youtube.com/embed/xvFZjo5PgG0"></iframe>',
+            "https://youtu.be/xvFZjo5PgG0",
+        ),
+        (
+            '<iframe width="560" height="315" src="https://youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            "https://youtu.be/xvFZjo5PgG0",
+        ),
+        (
+            '<iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>',
+            "https://youtu.be/xvFZjo5PgG0",
+        ),
+        (
+            '<iframe width="560" height="315" src="http://youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            "https://youtu.be/xvFZjo5PgG0",
+        ),
+        (
+            '<iframe width="560" height="315" src="https://cs50.harvard.edu/python"></iframe>',
+            None,
+        ),
+    ],
+)
+def test_parse(input, expected):
+    assert parse(input) == expected
---
+++ week7/watch/watch.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+# REGEXP = r'"(.*)"'
+REGEX_URL = r'src="((.*//)(.*youtube.com)(/embed.*?))"'
+REGEX_REPLACE = r'(.*//)(.*youtube.com)(/embed)(/.*?)'
+# src=\"(.*//(.*)/embed.*?)\"
+# REGEXP = r"\b(?:(?:25[0-5]?))\b"
+
+
+def parse(html):
+    res = re.search(REGEX_URL, html)
+    if not res:
+        return None
+    
+    res2 = re.sub(REGEX_REPLACE, r"https://youtu.be\4", res.groups()[0])
+    return res2
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+if __name__ == "__main__":
+    main()
