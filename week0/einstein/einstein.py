fff6bfb7d188bb4b64c8a2bcfe890e669b1e3eeb
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-06 10:43:29 +0300

    cleanup

--- week0/einstein/einstein.py
+++ week0/einstein/einstein.py
@@ -1,8 +1,10 @@
 CONST_C = 300000000
 
-def energy(mass:int) -> int:
-    return mass * (CONST_C ** 2)
+
+def energy(mass: int) -> int:
+    return mass * (CONST_C**2)
+
 
 if __name__ == "__main__":
-    user_input: int = int(input("Please neter mass (KG): "))
+    user_input: int = int(input("Please enter mass (KG): "))
     print(f"E: {energy(user_input):,}")
