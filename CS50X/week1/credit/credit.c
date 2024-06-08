06cdcd88620648cc22e39a75f3005d994ad524f2
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-08 04:08:34 +0300

    minor refactoring

--- CS50X/week1/credit/credit.c
+++ CS50X/week1/credit/credit.c
@@ -5,34 +5,35 @@
 #include <string.h>
 
 const string invalid = "INVALID";
-const string visa = "VISA";
-const string mastercard = "MASTERCARD";
-const string amex = "AMEX";
 
-const string regex_general = "\b(?:(?:(?:3[47])\\d{13})|(?:(?:5[12345])\\d{14})|(?:4(?:\\d{12}|\\d{15})))\b";
-const string regex_amex = "\b(?:3[47]\\d{13})\b";
-const string regex_mastercard = "\b(?:5[12345]\\d{14})\b";
-const string regex_visa = "\b(?:4\\d{12}|4\\d{15})\b";
-
-string cards[3][2] = {{"\\b(?:3[47]\\d{13})\\b", "AMEX"},
-                      {"\\b(?:5[12345]\\d{14})\\b", "MASTERCARD"},
-                      {"\\b(?:4\\d{12}|4\\d{15})\\b", "VISA"}};
+string cards[3][2] = {{"^(3[47][0-9]{13})$", "AMEX"},
+                      {"^(5[12345][0-9]{14})$", "MASTERCARD"},
+                      {"^(4[0-9]{12}|4[0-9]{15})$", "VISA"}};
 
 bool validate_card(string number)
 {
     int sum = 0;
-    for (int i = 0; i < strlen(number); i++)
+    int idx = 1;
+    for (int i = strlen(number) - 1; i >= 0; i--)
     {
+        // A cool way to convert char to nymber.
+        //  ASCII values of digits are sequential, starting from '0' (48) to '9' (57).
+        // So, subtracting the ASCII value of a digit character from '0'
+        // will give us its numeric value.
         int digit = number[i] - '0';
-        if (i % 2 == 0)
+        // printf("%d, %d\n", i, idx);
+        if (idx % 2 == 0)
         {
             digit *= 2;
+            // Fun way to get a sum of 2-digit number below 20
+            // e.g. 12 => 1+2 = 12-9 = 3
             if (digit > 9)
             {
                 digit -= 9;
             }
         }
         sum += digit;
+        idx++;
     }
     return sum % 10 == 0;
 }
@@ -40,23 +41,23 @@ bool validate_card(string number)
 bool validate_input(regex_t regex, string number)
 {
     int result = regexec(&regex, number, 0, NULL, 0);
-    if (result == 0)
-    {
-        return true;
-    }
 
-    return false;
+    return (result == 0);
 }
 
 string detect_carrier(string number)
 {
+    if (!validate_card(number))
+    {
+        return invalid;
+    }
+
     regex_t regex;
     for (int i = 0; i < 3; i++)
     {
-        printf("Regex: %s\n", cards[i][0]);
-        printf("regcomp: %d\n", regcomp(&regex, cards[i][0], 0));
-        int result = regexec(&regex, number, 0, NULL, 0);
-        printf("regcomp: %d\n", result);
+        int result = regcomp(&regex, cards[i][0], REG_EXTENDED | REG_NOSUB);
+
+        result = regexec(&regex, number, 0, NULL, 0);
         if (result == 0)
         {
             return cards[i][1];
@@ -71,21 +72,22 @@ int main(void)
     regex_t regex;
     int result;
     char regex_all[80];
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%s\n", cards[i][0]);
-    }
 
     sprintf(regex_all, "%s|%s|%s", cards[0][0], cards[1][0], cards[2][0]);
+    result = regcomp(&regex, "[0-9]{1,16}", REG_EXTENDED | REG_NOSUB);
 
-    // result = regcomp(&regex, regex_all, 0);
-    result = regcomp(&regex, "(?:(?:(?:37|34)\\d{13})|(?:(?:51|52|53|54|55)\\d{14})|(?:4(?:\\d{12}|\\d{15})))", 0);
+    if (result != 0)
+    {
+        printf("regcomp: %d\n", result);
+        return 1;
+    }
 
     string number = "";
     do
     {
         number = get_string("Number: ");
-    } while (!validate_input(regex, number));
+    } 
+    while (!validate_input(regex, number));
 
     printf("%s\n", detect_carrier(number));
 }
