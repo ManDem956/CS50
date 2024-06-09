e55a6bff725d28577bc5523be099bae6be956744
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-09 03:30:01 +0300

    include guards

--- CS50X/week1/readability/readability.h
+++ CS50X/week1/readability/readability.h
@@ -1,5 +1,11 @@
-#include <cs50.h>
+#ifndef _READABILITY_H_   /* Include guard */
+#define _READABILITY_H_
+
+
+
 
 int calculate(int wordCount, int letterCount, int sentenceCount);
 
 void stats(char *pattern, char *text, int cflags, int *buff);
+
+#endif // _READABILITY_H_
