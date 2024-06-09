e8d16e453d17661e285d38cbc504d793c8ac3743
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-09 21:32:50 +0300

    cleanup

--- CS50X/week1/substitution/src/substitution.c
+++ CS50X/week1/substitution/src/substitution.c
@@ -1,15 +1,13 @@
 
 #include <cs50.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <ctype.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_KEY_LENGTH 26
-#define VALIDATE "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// const string VALIDATE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string VALIDATE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 char *to_upper(string s);
 
@@ -29,20 +27,23 @@ char *encode(string s, string key)
             // We need this to factor in both lowercase and uppercase inputs
             int diff = result[i] - upper;
 
+            // Reducing the character's int representation to ordinal number in the alphabet 
+            // This allows us to ise this number as an index for the cipher key
             result[i] = key[upper - 'A'] + diff;
         }
     }
     return result;
 }
 
-char *to_upper(string s){
+char *to_upper(string s)
+{
     string result = malloc(sizeof(char) * strlen(s) + 1);
     strcpy(result, s);
-    for (int i = 0; i < strlen(result); i++){
+    for (int i = 0; i < strlen(result); i++)
+    {
         result[i] = toupper(result[i]);
     }
     return result;
-
 }
 
 void do_run(string key)
@@ -51,16 +52,16 @@ void do_run(string key)
     printf("ciphertext: %s\n", encode(text, to_upper(key)));
 }
 
-// int main(int argc, char **argv)
-// {
-//     if ((argc != 2) ||
-//         (strlen(argv[1]) != MAX_KEY_LENGTH) && (strspn(argv[1], VALIDATE) != MAX_KEY_LENGTH))
-//     {
-//         printf("Usage: ./caesar key\n\t key: a string of length 26 to base the "
-//                "substitution cipher on.\n");
-//         return 1;
-//     }
+int main(int argc, char **argv)
+{
+    if ((argc != 2) || (strlen(argv[1]) != MAX_KEY_LENGTH) ||
+        (strspn(VALIDATE, to_upper(argv[1])) != MAX_KEY_LENGTH))
+    {
+        printf("Usage: ./caesar key\n\t key: a string of length 26 to base the "
+               "substitution cipher on.\n");
+        return 1;
+    }
 
-//     string key = argv[1];
-//     do_run(key);
-// }
+    string key = argv[1];
+    do_run(key);
+}
