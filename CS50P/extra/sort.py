35befb960aca0c7b55c8f46b175ca74dbc00790a
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-10 11:19:49 +0300

    some extra tries

---
+++ CS50P/extra/sort.py
@@ -0,0 +1,39 @@
+import csv
+import random
+from typing import Iterable
+import pathlib
+
+
+BASE = 4000
+path = f"{pathlib.Path(__file__).parent.resolve()}/data"
+
+
+def gen_data(size: int, seed: int) -> None:
+    print()
+    random.seed(seed)
+    random_data = random.sample(range(size), size)
+
+    write_file(size, ((item,) for item in random_data), "random")
+    write_file(size, ((item,) for item in range(size)), "sorted")
+    write_file(size, ((item,) for item in range(size - 1, -1, -1)), "reversed")
+
+
+def write_file(size: int, the_list: Iterable, name) -> None:
+    filename =f"{path}/{name}-{size:>06}.txt" 
+    print(f"Writing {filename}")
+    with open(filename, "w") as csvfile:
+        sortwriter = csv.writer(
+            csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+
+        sortwriter.writerows(the_list)
+
+
+def main() -> None:
+
+    for i in range(6):
+        gen_data((2 ** i) * BASE, 1234)
+
+
+if __name__ == "__main__":
+    main()
