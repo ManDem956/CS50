f1a198ddd0cf4096ee2118ebddd6d8c6b19e8b46
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-06-10 22:46:51 +0000

    Tue, Jun 11, 2024, 1:46 AM +03:00

--- CS50X/week3/sort/run.sh
+++ CS50X/week3/sort/run.sh
@@ -57,7 +57,7 @@ for exec in ./sort*; do
 
         for ((i = 1; i <= RUNS; i++)); do
             echo -ne "Run ${i}/$RUNS"\\r
-            time -o "$outfile" -a -f "${exec##*/}|${outfilename%%+([0-9])}|${outfilename##+([a-z])}|%e|%P|%k|%x" "${COMMAND[@]}" >/dev/null 2>&1
+            /usr/bin/time -o "$outfile" -a -f "${exec##*/}|${outfilename%%+([0-9])}|${outfilename##+([a-z])}|%e|%P|%k|%x" "${COMMAND[@]}" >/dev/null 2>&1
             # sleep 10
         done
 
