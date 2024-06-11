5978df2861740fc84ef0972c4a2e476ef7023e03
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-11 17:15:11 +0300

    mergesort impl

--- cs50x/week3/sort/practice/merge.c
+++ cs50x/week3/sort/practice/merge.c
@@ -14,31 +14,74 @@ void swap(int *xp, int *yp)
 
 void printArray(int arr[], int size)
 {
-    int i;
-    for (i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
+void merge(int arr[], int size, int left, int mid, int right)
+{
+    int idx_left, idx_right, idx_result;
+    int size_left = mid - left + 1;
+    int size_right = right - mid;
+
+    int left_array[size_left], right_array[size_right];
+
+    for (idx_left = 0; idx_left < size_left; idx_left++)
+    {
+        left_array[idx_left] = arr[left + idx_left];
+    }
+    for (idx_right = 0; idx_right < size_right; idx_right++)
+    {
+        right_array[idx_right] = arr[mid + 1 + idx_right];
+    }
+
+    idx_left = 0;
+    idx_right = 0;
+    idx_result = left;
+    while (idx_left < size_left && idx_right < size_right)
+    {
+        if (left_array[idx_left] <= right_array[idx_right])
+        {
+            arr[idx_result] = left_array[idx_left];
+            idx_left++;
+        }
+        else
+        {
+            arr[idx_result] = right_array[idx_right];
+            idx_right++;
+        }
+        idx_result++;
+    }
+
+    while (idx_left < size_left)
+    {
+        arr[idx_result] = left_array[idx_left];
+        idx_left++;
+        idx_result++;
+    }
+    while (idx_right < size_right)
+    {
+        arr[idx_result] = right_array[idx_right];
+        idx_right++;
+        idx_result++;
+    }
+}
+
+void mergesort(int arr[], int size, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        mergesort(arr, size, left, mid);
+        mergesort(arr, size, mid + 1, right);
+        merge(arr, size, left, mid, right);
+    }
+}
+
 void do_sort(size_t size, int n[])
 {
-    int i;
-    bool swapped;
-    do
-    {
-        i = size - 1;
-        swapped = false;
-        while (i > 0)
-        {
-            if (n[i - 1] > n[i])
-            {
-                swap(&n[i - 1], &n[i]);
-                swapped = true;
-            }
-            i--;
-        }
-    }
-    while (swapped);
+    mergesort(n, size, 0, size - 1);
 }
 
 void do_run()
