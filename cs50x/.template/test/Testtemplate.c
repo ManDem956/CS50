b500ce43176f0e6218d7a8c3406938485109e173
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-11 19:07:20 +0300

    update template design

---
+++ cs50x/.template/test/Testtemplate.c
@@ -0,0 +1,28 @@
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../src/template.h"
+#include "../unity/src/unity.h"
+
+void test_dummy(void)
+{
+    TEST_ASSERT_EQUAL_STRING("test", "test");
+}
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_dummy);
+    UNITY_END();
+}
+
