95136f99fb4cdbeb4eefb0f697b8f51022a7845f
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-20 11:30:58 +0300

    working - add negative tests

--- cs50p/week7/working/test_working.py
+++ cs50p/week7/working/test_working.py
@@ -11,6 +11,8 @@ from contextlib import nullcontext as does_not_raise
         ("10 PM to 8 AM", "22:00 to 08:00", does_not_raise()),
         ("10:00 PM to 8:00 AM", "22:00 to 08:00", does_not_raise()),
         ("9:60 AM to 5:59 PM", None, pytest.raises(ValueError)),
+        ("9:59 AM to 5:60 PM", None, pytest.raises(ValueError)),
+        ("9:59 AM to 5:600 PM", None, pytest.raises(ValueError)),
         ("9 AM - 5 PM", None, pytest.raises(ValueError)),
         ("09:00 AM - 17:00 PM", None, pytest.raises(ValueError)),
         ("cat", None, pytest.raises(ValueError)),
