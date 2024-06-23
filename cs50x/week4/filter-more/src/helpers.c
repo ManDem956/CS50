96ff594ff9d3a5925365e82e1afb6bdab96daf5c
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-23 03:58:37 +0300

    week4 filter - more

--- cs50x/week4/filter-more/src/helpers.c
+++ cs50x/week4/filter-more/src/helpers.c
@@ -1,25 +1,164 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            BYTE rgbtAverage =
+                round((image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3.0);
+            image[h][w].rgbtBlue = image[h][w].rgbtGreen = image[h][w].rgbtRed = rgbtAverage;
+        }
+    }
+}
+
+void swap(RGBTRIPLE *left, RGBTRIPLE *right)
+{
+    RGBTRIPLE tmp = *left;
+    *left = *right;
+    *right = tmp;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int h = 0; h < height; h++)
+    {
+        int left = 0;
+        int right = width - 1;
+        while (left < right)
+        {
+            swap(&image[h][left], &image[h][right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+RGBTRIPLE blur_pixel(int height, int width, RGBTRIPLE image[height][width], int row, int col)
+{
+    RGBTRIPLE result = {0, 0, 0};
+    double red = 0;
+    double green = 0;
+    double blue = 0;
+
+    int pixelCount = 0;
+
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            int currentRow = row + i;
+            int currentCol = col + j;
+
+            if (currentRow >= 0 && currentRow < height && currentCol >= 0 && currentCol < width)
+            {
+                red += image[currentRow][currentCol].rgbtRed;
+                green += image[currentRow][currentCol].rgbtGreen;
+                blue += image[currentRow][currentCol].rgbtBlue;
+
+                pixelCount++;
+            }
+        }
+    }
+
+    result.rgbtRed = round(red / (double) pixelCount);
+    result.rgbtGreen = round(green / (double) pixelCount);
+    result.rgbtBlue = round(blue / (double) pixelCount);
+
+    return result;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    RGBTRIPLE tmp[height][width];
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            tmp[h][w] = blur_pixel(height, width, image, h, w);
+        }
+    }
+
+    // Swap the image and tmp array
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            image[h][w] = tmp[h][w];
+        }
+    }
 }
 
+RGBTRIPLE sobel_sum(int height, int width, RGBTRIPLE image[height][width], int row, int col)
+{
+    RGBTRIPLE result = {0, 0, 0};
+    int gx_kernel[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int gy_kernel[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    double gx_red = 0;
+    double gx_green = 0;
+    double gx_blue = 0;
+
+    double gy_red = 0;
+    double gy_green = 0;
+    double gy_blue = 0;
+
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            int currentRow = row + i;
+            int currentCol = col + j;
+
+            double red = 0;
+            double green = 0;
+            double blue = 0;
+            if (currentRow >= 0 && currentRow < height && currentCol >= 0 && currentCol < width)
+            {
+                RGBTRIPLE current_pixel = image[currentRow][currentCol];
+                gx_red += gx_kernel[i + 1][j + 1] * current_pixel.rgbtRed;
+                gx_green += gx_kernel[i + 1][j + 1] * current_pixel.rgbtGreen;
+                gx_blue += gx_kernel[i + 1][j + 1] * current_pixel.rgbtBlue;
+
+                gy_red += gy_kernel[i + 1][j + 1] * current_pixel.rgbtRed;
+                gy_green += gy_kernel[i + 1][j + 1] * current_pixel.rgbtGreen;
+                gy_blue += gy_kernel[i + 1][j + 1] * current_pixel.rgbtBlue;
+            }
+        }
+    }
+    double g_root_red = sqrt(gx_red * gx_red + gy_red * gy_red);
+    double g_root_green = sqrt(gx_green * gx_green + gy_green * gy_green);
+    double g_root_blue = sqrt(gx_blue * gx_blue + gy_blue * gy_blue);
+
+    result.rgbtRed = (g_root_red > 255) ? 255 : round(g_root_red);
+    result.rgbtGreen = (g_root_green > 255) ? 255 : round(g_root_green);
+    result.rgbtBlue = (g_root_blue > 255) ? 255 : round(g_root_blue);
+
+    return result;
+}
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    RGBTRIPLE tmp[height][width];
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            tmp[h][w] = sobel_sum(height, width, image, h, w);
+        }
+    }
+
+    // Swap the image and tmp array
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            image[h][w] = tmp[h][w];
+        }
+    }
 }
--- cs50x/week4/filter-more/test/Testfilter.c
+++ cs50x/week4/filter-more/test/Testfilter.c
@@ -1,18 +1,167 @@
 
 #include <cs50.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "../src/helpers.h"
 #include "../unity/src/unity.h"
 
-void test_dummy(void)
+int height = 3;
+int width = 3;
+
+RGBTRIPLE (*image)[3];
+
+void check_image_pixel(RGBTRIPLE image[3][3], int row, int col, uint8_t expectedRed,
+                       uint8_t expectedGreen, uint8_t expectedBlue);
+void initializeImagePixels_blur(RGBTRIPLE image[3][3], int numRows, int numColumns);
+void initializeImagePixels_edges(RGBTRIPLE image[3][3]);
+
+void test_edges_filter()
 {
-    TEST_ASSERT_EQUAL_STRING("test", "test");
+    initializeImagePixels_edges(image);
+
+    if (image == NULL)
+    {
+        TEST_FAIL_MESSAGE("Image is not initialized.");
+    }
+
+    if (height < 3 || width < 3)
+    {
+        TEST_FAIL_MESSAGE("Invalid image dimensions.");
+    }
+
+    edges(height, width, image);
+    // check_image_pixel(image, 0, 1, 213, 228, 255);
+    check_image_pixel(image, 1, 1, 210, 150, 60);
+}
+
+void check_image_pixel(RGBTRIPLE image[3][3], int row, int col, uint8_t expectedRed,
+                       uint8_t expectedGreen, uint8_t expectedBlue)
+{
+    TEST_ASSERT_EQUAL_UINT8(expectedRed, image[row][col].rgbtRed);
+    TEST_ASSERT_EQUAL_UINT8(expectedGreen, image[row][col].rgbtGreen);
+    TEST_ASSERT_EQUAL_UINT8(expectedBlue, image[row][col].rgbtBlue);
+}
+
+void test_blur_filter(void)
+{
+    initializeImagePixels_blur(image, height, width);
+    // Check if image is initialized before using it
+    if (image == NULL)
+    {
+        TEST_FAIL_MESSAGE("Image is not initialized.\n");
+    }
+
+    // Check if image is not out of bounds
+    if (height < 3 || width < 3)
+    {
+        TEST_FAIL_MESSAGE("Image dimensions are invalid.\n");
+    }
+
+    blur(height, width, image);
+    check_image_pixel(image, 1, 1, 127, 140, 149);
 }
 
 void setUp(void)
 {
+    int numRows = 3;
+    int numColumns = 3;
+
+    image = calloc(numRows, numColumns * sizeof(RGBTRIPLE));
+    if (image == NULL)
+    {
+        printf("Failed to allocate memory for image.\n");
+        exit(1);
+    }
+}
+
+void initializeImagePixels_blur(RGBTRIPLE image[3][3], int rows, int cols)
+{
+    // first row: (10, 20, 30), (40, 50, 60), (70, 80, 90)
+    // second row: (110, 130, 140), (120, 140, 150), (130, 150, 160)
+    // third row: (200, 210, 220), (220, 230, 240), (240, 250, 255)
+    image[0][0].rgbtRed = 10;
+    image[0][0].rgbtGreen = 20;
+    image[0][0].rgbtBlue = 30;
+
+    image[0][1].rgbtRed = 40;
+    image[0][1].rgbtGreen = 50;
+    image[0][1].rgbtBlue = 60;
+
+    image[0][2].rgbtRed = 70;
+    image[0][2].rgbtGreen = 80;
+    image[0][2].rgbtBlue = 90;
+
+    image[1][0].rgbtRed = 110;
+    image[1][0].rgbtGreen = 130;
+    image[1][0].rgbtBlue = 140;
+
+    image[1][1].rgbtRed = 120;
+    image[1][1].rgbtGreen = 140;
+    image[1][1].rgbtBlue = 150;
+
+    image[1][2].rgbtRed = 130;
+    image[1][2].rgbtGreen = 150;
+    image[1][2].rgbtBlue = 160;
+
+    image[2][0].rgbtRed = 200;
+    image[2][0].rgbtGreen = 210;
+    image[2][0].rgbtBlue = 220;
+
+    image[2][1].rgbtRed = 220;
+    image[2][1].rgbtGreen = 230;
+    image[2][1].rgbtBlue = 240;
+
+    image[2][2].rgbtRed = 240;
+    image[2][2].rgbtGreen = 250;
+    image[2][2].rgbtBlue = 255;
+}
+void initializeImagePixels_edges(RGBTRIPLE image[3][3])
+{
+    // first row: (0, 10, 25), (0, 10, 30), (40, 60, 80)
+    // second row: (20, 30, 90), (30, 40, 100), (80, 70, 90)
+    // third row: (20, 20, 40), (30, 10, 30), (50, 40, 10)
+
+    // Define colors for each pixel
+    // (0, 10, 25);
+    image[0][0].rgbtRed = 0;
+    image[0][0].rgbtGreen = 10;
+    image[0][0].rgbtBlue = 25;
+
+    // (0, 10, 30)
+    image[0][1].rgbtRed = 0;
+    image[0][1].rgbtGreen = 10;
+    image[0][1].rgbtBlue = 30;
+    // (40, 60, 80)
+    image[0][2].rgbtRed = 40;
+    image[0][2].rgbtGreen = 60;
+    image[0][2].rgbtBlue = 80;
+    // (20, 30, 90)
+    image[1][0].rgbtRed = 20;
+    image[1][0].rgbtGreen = 30;
+    image[1][0].rgbtBlue = 90;
+    // (30, 40, 100),
+    image[1][1].rgbtRed = 30;
+    image[1][1].rgbtGreen = 40;
+    image[1][1].rgbtBlue = 100;
+    // (80, 70, 90)
+    image[1][2].rgbtRed = 80;
+    image[1][2].rgbtGreen = 70;
+    image[1][2].rgbtBlue = 90;
+    // (20, 20, 40),
+    image[2][0].rgbtRed = 20;
+    image[2][0].rgbtGreen = 20;
+    image[2][0].rgbtBlue = 40;
+    // (30, 10, 30), (50, 40, 10)
+    image[2][1].rgbtRed = 30;
+    image[2][1].rgbtGreen = 10;
+    image[2][1].rgbtBlue = 30;
+    // (50, 40, 10)
+    image[2][2].rgbtRed = 50;
+    image[2][2].rgbtGreen = 40;
+    image[2][2].rgbtBlue = 10;
 }
 
 void tearDown(void)
@@ -22,7 +171,7 @@ void tearDown(void)
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_dummy);
+    RUN_TEST(test_edges_filter);
+    RUN_TEST(test_blur_filter);
     UNITY_END();
 }
-
