d8995d59cb5f3e19af5758ed5031a0dd0808c8dd
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-06-24 08:17:04 +0300

    cleanup tests

--- cs50p/week9/project/test_project.py
+++ cs50p/week9/project/test_project.py
@@ -34,6 +34,7 @@ class TestHelpers:
     def test_reshape_to_2d(self, input, size, expected):
         assert reshape_to_2d(input, size).tolist() == expected
 
+    # fmt: off
     @pytest.mark.parametrize(
         "array_nxm, size, expected",
         [
@@ -46,40 +47,20 @@ class TestHelpers:
                 {"size": 3, "dimension": 3},
                 3,
                 [
-                    (0, 4, 8),
-                    (0, 10, 20),
-                    (0, 12, 24),
-                    (0, 13, 26),
-                    (1, 13, 25),
-                    (2, 14, 26),
-                    (3, 13, 23),
-                    (6, 16, 26),
-                    (9, 13, 17),
-                    (18, 22, 26),
+                    (0, 4, 8), (0, 10, 20), (0, 12, 24), (0, 13, 26), (1, 13, 25), (2, 14, 26), (3, 13, 23), (6, 16, 26), (9, 13, 17), (18, 22, 26),  # noqa
                 ],
             ),
             (
                 {"size": 4, "dimension": 3},
                 4,
                 [
-                    (0, 5, 10, 15),
-                    (0, 17, 34, 51),
-                    (0, 20, 40, 60),
-                    (0, 21, 42, 63),
-                    (1, 21, 41, 61),
-                    (2, 22, 42, 62),
-                    (3, 23, 43, 63),
-                    (4, 21, 38, 55),
-                    (8, 25, 42, 59),
-                    (12, 29, 46, 63),
-                    (16, 21, 26, 31),
-                    (32, 37, 42, 47),
-                    (48, 53, 58, 63),
+                    (0, 5, 10, 15), (0, 17, 34, 51), (0, 20, 40, 60), (0, 21, 42, 63), (1, 21, 41, 61), (2, 22, 42, 62), (3, 23, 43, 63), (4, 21, 38, 55), (8, 25, 42, 59), (12, 29, 46, 63), (16, 21, 26, 31), (32, 37, 42, 47), (48, 53, 58, 63), # noqa
                 ],
             ),
         ],
         indirect=["array_nxm"],
     )
+    # fmt: on
     def test_generate_diagonals(self, array_nxm, size, expected):
         actual = self.get_set_from_list(generate_diagonals(array_nxm, size).tolist())
         expected = self.get_set_from_list(expected)
