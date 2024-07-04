040e1816f3a969fab6a7524c4175107e51302520
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-07-04 04:59:51 +0300

    experiments

--- cs50p/week8/jar/jar.py
+++ cs50p/week8/jar/jar.py
@@ -9,17 +9,20 @@ class Jar:
         return "🍪" * self.__count
 
     def deposit(self, n: int) -> None:
-        if not (0 <= self.__count + n <= self.__capacity):
+        if not self.__check_bounds(n):
             raise ValueError("Can not deposit {n} cookies, jar is full")
 
         self.__count += n
 
     def withdraw(self, n: int) -> None:
-        if not (0 <= self.__count - n <= self.__capacity):
+        if not self.__check_bounds(n*(-1)):
             raise ValueError("Can not withdraw {n} cookies, jar is empty")
 
         self.__count -= n
 
+    def __check_bounds(self, n: int) -> bool:
+        return (0 <= self.__count + n <= self.__capacity)
+    
     @property
     def capacity(self) -> int:
         return self.__capacity
