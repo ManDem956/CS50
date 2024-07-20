75a24960750dfab19be7c67dec292f2ea2c59243
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-07-20 18:43:24 +0300

    pizza - cleanup

--- cs50p/week6/pizza/pizza.py
+++ cs50p/week6/pizza/pizza.py
@@ -10,19 +10,24 @@ from tabulate import tabulate
 def build_grid(filename: str) -> int:
     with open(filename) as file:
         reader = csv.reader(file)
+        # tabulate can just accept reader, no need to biffer in the form of a python collection
         return tabulate(reader, tablefmt="grid", headers="firstrow")
 
 
 def validate(filename: str) -> NoReturn:
+    """Validate the file. file.stat() will throw the FileNotFoundError"""
     file = Path(filename)
-    if not file.is_file():
-        raise FileNotFoundError(f"File not found {file.absolute()}")
+
+    try:
+        if file.stat().st_size <= 0:
+            raise ValueError(f"{file.absolute()} is an empty file.")
+    except FileNotFoundError as e:
+        # replae file name with absolute path
+        msg = str(e).replace(filename, str(file.absolute()))
+        raise FileNotFoundError(msg)
 
     if file.suffix != ".csv":
-        raise ValueError(f"{file.absolute()} is not a csv file")
-
-    if file.stat().st_size <= 0:
-        raise ValueError(f"{file.absolute()} is empty")
+        raise ValueError(f"{file.absolute()} is not a csv file.")
 
 
 def main() -> None:
