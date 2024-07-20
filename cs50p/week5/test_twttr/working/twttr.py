a2bda0a2bf04b5cf406c816edd4811c2c05a2db8
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-07-21 00:26:01 +0300

    twttr cleanup

---
+++ cs50p/week5/test_twttr/working/twttr.py
@@ -0,0 +1,26 @@
+CONST_USER_PROMPT = "Input"
+VOWELS = "aeiou"
+
+
+def shorten(word: str) -> str:
+    res = ""
+    for char in word:
+        if char.lower() not in VOWELS:
+            res += char
+
+    return res
+
+
+def get_user_input(str) -> str:
+    res: str = input(f"{str}: ").strip()
+    return res
+
+
+def main() -> None:
+    input = get_user_input(CONST_USER_PROMPT)
+    result = shorten(input)
+    print(f"Output: {result}")
+
+
+if __name__ == "__main__":
+    main()
