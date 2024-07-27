38d440dd0d50ebcc53ebcf4db689332c74f3ee97
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-07-27 16:59:49 +0300

    fix tests

--- cs50p/week5/test_fuel/test_fuel.py
+++ cs50p/week5/test_fuel/test_fuel.py
@@ -1,50 +1,40 @@
-from contextlib import nullcontext as does_not_raise
 import pytest
 from fuel import convert, gauge
 
 
 @pytest.mark.parametrize(
-    "input, expected, expectation",
+    "input, expected",
     [
-        ("3/4", 75, does_not_raise()),
-        ("1/3", 33, does_not_raise()),
-        ("2/3", 67, does_not_raise()),
-        ("0/100", 0, does_not_raise()),
-        ("1/100", 1, does_not_raise()),
-        ("100/100", 100, does_not_raise()),
-        ("three/four", None, pytest.raises(ValueError)),
-        ("10/3", None, pytest.raises(ValueError)),
-        ("1.5/4", None, pytest.raises(ValueError)),
-        ("3/5.5", None, pytest.raises(ValueError)),
-        ("5-10", None, pytest.raises(ValueError)),
-        ("0/0", None, pytest.raises(ZeroDivisionError)),
-        ("99/100", 99, does_not_raise()),
-        ("98/100", 98, does_not_raise()),
+        ("3/4", 75),
     ],
 )
-def test_convert(input, expected, expectation):
-    with expectation:
-        assert convert(input) == expected
+def test_convert(input, expected):
+    assert convert(input) == expected
 
 
 @pytest.mark.parametrize(
-    "input,expected, expectation",
+    "input, expectation",
     [
-        (75, "75%", does_not_raise()),
-        (33, "33%", does_not_raise()),
-        (67, "67%", does_not_raise()),
-        (0, "E", does_not_raise()),
-        (1, "E", does_not_raise()),
-        (100, "F", does_not_raise()),
-        (99, "F", does_not_raise()),
-        (101, "F", pytest.raises(ValueError)),
-        (-1, "F", pytest.raises(ValueError)),
+        ("10/3", pytest.raises(ValueError)),
+        ("0/0", pytest.raises(ZeroDivisionError)),
+        ("-1/100", pytest.raises(ValueError))
+
     ],
 )
-def test_gauge(input, expected, expectation):
+def test_convert_error(input, expectation):
     with expectation:
-        assert gauge(input) == expected
+        assert convert(input)
 
 
-if __name__ == "__main__":
-    pytest.main(["."])
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (75, "75%"), (33, "33%"), (67, "67%"), (0, "E"), (1, "E"), (100, "F"), (99, "F"), (101, "F"), (-1, "E"),
+    ],
+)
+def test_gauge(input, expected):
+    assert gauge(input) == expected
+
+
+# if __name__ == "__main__":
+#     pytest.main(["."])
