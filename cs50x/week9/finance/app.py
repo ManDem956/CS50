762850c4b8a926b09207987c2330c478ac36c82d
Kostiantyn Gurianov <kguryanov@users.noreply.github.com>
2024-07-29 08:24:29 +0000

    Mon, Jul 29, 2024, 11:24 AM +03:00

--- cs50x/week9/finance/app.py
+++ cs50x/week9/finance/app.py
@@ -83,7 +83,7 @@ def index():
     try:
         result = db.execute(SQL_GET_STOCKS, session["user_id"])
         cash = db.execute(SQL_GET_USER_CASH, session["user_id"])[0]["cash"]
-    except (RuntimeError, ValueError) as e:
+   except (RuntimeError, ValueError) as e:
         do_report(str(e))
 
     for res in result:
