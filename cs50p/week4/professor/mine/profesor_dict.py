f84e7c56ca448c1a71445a006aee44ac622b7f25
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-08-02 16:29:20 +0300

    cs50p - week4 - professor - refactor

--- cs50p/week4/professor/mine/profesor_dict.py
+++ cs50p/week4/professor/mine/profesor_dict.py
@@ -20,17 +20,20 @@ def get_level(message: str = CONST_USER_PROMPT_LEVEL) -> int:
     return result
 
 
-def generate_integer(level: int) -> int:
+def generate_integer(level: int) -> list[tuple[int, int]]:
     if level not in CONST_ALLOW_RANGE:
         raise ValueError(f"Level must in range {list(CONST_ALLOW_RANGE)}")
     the_range = (10**level,) if level <= 1 else (10 ** (level - 1), (10**level)-1)
-    print(the_range)
-    return random.randrange(*the_range)
+
+    result = []
+    for _ in range(10):
+        result.append((random.randrange(*the_range), random.randrange(*the_range)))
+    return result
 
 
-def do_game(level) -> int:
+def do_game(left, right) -> int:
     tries = 3
-    left, right = generate_integer(level), generate_integer(level)
+    # left, right = generate_integer(level), generate_integer(level)
     while tries > 0:
         try:
             answer = int(get_user_input(f"{left} + {right} =", sep=" "))
@@ -48,11 +51,13 @@ def do_game(level) -> int:
 
 
 def main() -> None:
-    # random.seed(0)
+    random.seed(0)
     level = get_level(CONST_USER_PROMPT_LEVEL)
     score = 0
-    for _ in range(10):
-        score += do_game(level)
+    values = generate_integer(level)
+    print(values)
+    for left, right in values:
+        score += do_game(left, right)
 
     print(f"Score: {score}")
 
