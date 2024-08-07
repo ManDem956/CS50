f6db0179f8a9ba9a12db7208b3f35d69ae583172
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-08-07 08:55:41 +0300

    add cach balance for history - finance

--- cs50x/week9/finance/app.py
+++ cs50x/week9/finance/app.py
@@ -44,11 +44,17 @@ GROUP BY user_id,
     symbol
 HAVING symbol NOT NULL and sum(quantity) > 0 and user_id = ?"""
 
-SQL_USER_HISTORY = """SELECT DATETIME(ut.transation_time, 'localtime') time, tt.name,
+SQL_USER_HISTORY = """SELECT DATETIME(ut.transation_time, 'localtime') time,
+    tt.name,
     ut.symbol,
     ut.quantity,
     ut.price,
-    ut.amount
+    ut.amount,
+    CASE
+        tt.type
+        WHEN 'credit' THEN ut.amount * -1
+        ELSE ut.amount
+    END balance_amount
 FROM user_transaction ut
     JOIN transaction_type tt ON tt.id = ut.transation_type_id
 WHERE ut.user_id = ?
--- cs50x/week9/finance/sql/history.sql
+++ cs50x/week9/finance/sql/history.sql
@@ -1,9 +1,15 @@
-SELECT DATETIME(ut.transation_time, 'localtime') time, tt.name,
+SELECT DATETIME(ut.transation_time, 'localtime') time,
+    tt.name,
     ut.symbol,
     ut.quantity,
     ut.price,
-    ut.amount
+    ut.amount,
+    CASE
+        tt.type
+        WHEN 'credit' THEN ut.amount * -1
+        ELSE ut.amount
+    END balance_amount
 FROM user_transaction ut
     JOIN transaction_type tt ON tt.id = ut.transation_type_id
-WHERE ut.user_id = 1
+WHERE ut.user_id = 2
 ORDER BY transation_time ASC
