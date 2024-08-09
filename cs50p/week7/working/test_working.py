35ce67d197819a9e9faa50a584deb0f1df21ef03
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-08-09 11:27:51 +0300

    cleanup tests

--- cs50p/week7/working/test_working.py
+++ cs50p/week7/working/test_working.py
@@ -1,23 +1,32 @@
 import pytest
 from working import convert
-from contextlib import nullcontext as does_not_raise
+# from contextlib import nullcontext as does_not_raise
 
 
 @pytest.mark.parametrize(
-    "input, expected, exception",
+    "input, expected",
     [
-        ("9:00 AM to 5:00 PM", "09:00 to 17:00", does_not_raise()),
-        ("9 AM to 5 PM", "09:00 to 17:00", does_not_raise()),
-        ("10 PM to 8 AM", "22:00 to 08:00", does_not_raise()),
-        ("10:00 PM to 8:00 AM", "22:00 to 08:00", does_not_raise()),
-        ("9:60 AM to 5:59 PM", None, pytest.raises(ValueError)),
-        ("9:59 AM to 5:60 PM", None, pytest.raises(ValueError)),
-        ("9:59 AM to 5:600 PM", None, pytest.raises(ValueError)),
-        ("9 AM - 5 PM", None, pytest.raises(ValueError)),
-        ("09:00 AM - 17:00 PM", None, pytest.raises(ValueError)),
-        ("cat", None, pytest.raises(ValueError)),
+        ("9:00 AM to 5:00 PM", "09:00 to 17:00"),
+        ("9 AM to 5 PM", "09:00 to 17:00"),
+        ("10 PM to 8 AM", "22:00 to 08:00"),
+        ("10:00 PM to 8:00 AM", "22:00 to 08:00"),
     ],
 )
-def test_convert(input, expected, exception):
-    with exception:
-        assert convert(input) == expected
+def test_convert(input, expected):
+    assert convert(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input",
+    [
+        ("9:60 AM to 5:59 PM"),
+        ("9:59 AM to 5:60 PM"),
+        ("9:59 AM to 5:600 PM"),
+        ("9 AM - 5 PM"),
+        ("09:00 AM - 17:00 PM"),
+        ("cat"),
+    ],
+)
+def test_convert_errors(input):
+    with pytest.raises(ValueError):
+        convert(input)
