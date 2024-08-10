7721e3164a50640fec79228484aa05a2d90b9c7d
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-08-10 13:24:51 +0300

    remove week9

--- cs50p/week9/project-backup/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import abstracts
-from .game import Game
-from .cell import Cell
-from .player import RandomPlayer
-from .board import Board
-
-
-__all__ = ["abstracts", "Board", "RandomPlayer", "Game", "Cell"]
