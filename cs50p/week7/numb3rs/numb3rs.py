fa8a3b53c10624fc702de6605c205756242a3018
Kostiantyn Gurianov <kguryanov@gmail.com>
2024-08-12 13:14:57 +0300

    update numbers

--- cs50p/week7/numb3rs/numb3rs.py
+++ cs50p/week7/numb3rs/numb3rs.py
@@ -1,12 +1,11 @@
 import re
 
-REGEXP = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-
-# REGEXP = r"\b(?:(?:25[0-5]?))\b"
+# REGEX = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+REGEX = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b\.?){4}\b"
 
 
 def validate(ip):
-    return re.fullmatch(REGEXP, ip) is not None
+    return re.fullmatch(REGEX, ip) is not None
 
 
 def main() -> None:
